2efe37c7dfb557240cb98f9c24c4f57145c26683
keszigabesz <91908624+keszigabesz@users.noreply.github.com>
2022-05-27 13:13:55 +0200

    base service kész

--- frontend/micro-lab/src/app/service/physician.service.ts
+++ frontend/micro-lab/src/app/service/physician.service.ts
@@ -3,16 +3,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Physician } from '../model/physician';
+import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PhysicianService {
-  apiUrl: string = environment.apiUrl;
-  endPoint: string = 'physicians.json';
-  constructor(private http: HttpClient) {}
-  getAll(): Observable<Physician[]> {
-    return this.http.get<Physician[]>(`${this.apiUrl}${this.endPoint}`);
+export class PhysicianService extends BaseService<Physician> {
+
+  constructor(public override http: HttpClient) {
+    super(http);
+    this.entityName = 'physicians.json';
   }
+
 
 }
