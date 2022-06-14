8ab30b0a0e9d5ba25cde6f19817ed5a65990667a
keszigabesz <91908624+keszigabesz@users.noreply.github.com>
2022-06-14 15:19:10 +0200

    már majdnem kész az autentikáció

---
+++ frontend/src/app/model/user.ts
@@ -0,0 +1,7 @@
+export class User {
+  _id?: string = '';
+  userName: string = '';
+  fullName: string = '';
+  password?: string = '';
+  role?: number = 1;
+}
--- frontend/src/app/page/login/login.component.ts
+++ frontend/src/app/page/login/login.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService, ILoginData } from 'src/app/service/auth.service';
 import { ConfigService, IMenuItem } from 'src/app/service/config.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   sidebarMenuItems: IMenuItem[] = this.config.sidebarMenu;
 
-  constructor(private config: ConfigService) { }
+  loginData: ILoginData = {};
 
-  ngOnInit(): void {
+  constructor(private config: ConfigService, private auth: AuthService,) {}
+
+  ngOnInit(): void {}
+
+  onLogin(): void {
+    this.auth.login(this.loginData);
   }
-
 }
