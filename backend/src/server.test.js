1efc641381c8b219396aa7d624853fbd9ebf554d
keszigabesz <91908624+keszigabesz@users.noreply.github.com>
2022-06-16 13:45:54 +0200

    nem működik

---
+++ backend/src/server.test.js
@@ -0,0 +1,34 @@
+const app = require('./server');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const config = require('config');
+const Test = require('./model/test');
+const { response } = require('jest-mock-req-res');
+
+describe('REST API integration tests', () => {
+    beforeEach(done => {
+        const { host, user, pass } = config.get('database');
+        mongoose.connect(`mongodb+srv://${host}`, {
+            user,
+            pass,
+        }).then(conn => {
+            console.log('Mock database connection success!');
+            done();
+        })
+        .catch(err => {
+            throw new Error(err.message);
+        });
+    });
+
+    afterEach( done => {
+        mongoose.connection.close( () => done() );
+    });
+
+    test('GET /test-card', done => {
+       supertest(app).get('/test-card').expect(200)
+        .then(response => {
+            expect(Array.isArray(response.body)).toBeTruthy();
+            done();
+        });
+    });
+});
